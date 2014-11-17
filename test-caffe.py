7b5b195b8fb45e553dd4e50bee01e2a3c80b5ed2
geekan <geekan@foxmail.com>
2014-11-17 15:02:34 +0800

    test-caffe: test it

---
+++ test-caffe.py
@@ -0,0 +1,32 @@
+#coding:utf-8
+
+from matplotlib import pyplot
+import caffe
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+caffe_root = '/Users/wuchenglin/git/caffe/'
+
+# Set the right path to your model file, pretrained model
+# and the image you would like to classify.
+MODEL_FILE = caffe_root + 'models/bvlc_reference_caffenet/deploy.prototxt'
+PRETRAINED = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel'
+IMAGE_FILE = caffe_root + 'examples/images/cat.jpg'
+
+
+net = caffe.Classifier(MODEL_FILE, PRETRAINED,
+                       mean=np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy'),
+                       channel_swap=(2,1,0),
+                       raw_scale=255,
+                       image_dims=(256, 256))
+
+input_image = caffe.io.load_image(IMAGE_FILE)
+plt.imshow(input_image)
+
+prediction = net.predict([input_image])  # predict takes any number of images, and formats them for the Caffe net automatically
+print 'prediction shape:', prediction[0].shape
+plt.plot(prediction[0])
+print 'predicted class:', prediction[0].argmax()
+
+
